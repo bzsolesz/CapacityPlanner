5d3aac5be13e7c82a90d0cc4678d36ff98ef1892
bzsolesz <bzsolesz@users.noreply.github.com>
2017-05-28 18:34:26 +0100

    Spring actuator was added back + some configuration change on swagger

--- children-service/src/main/java/com/plm/children/Application.java
+++ children-service/src/main/java/com/plm/children/Application.java
@@ -7,9 +7,13 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -26,9 +30,19 @@ public class Application extends SpringBootServletInitializer {
 
     @Bean
     public Docket swaggerConfiguration() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(cretaeChildrenServiceApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.plm.children"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo cretaeChildrenServiceApiInfo() {
+        return new ApiInfo(
+                "API Documentation of Children Service",
+                "API Documentation of service methods dealing with Children Information"
+                , "", "", null, "", "", new ArrayList<>());
     }
 }
