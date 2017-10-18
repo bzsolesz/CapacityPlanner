576b0f1b2bb8eca4673d08fc50ab5667cf18800a
bzsolesz <zsolt.balogh99@gmail.com>
2017-10-19 00:09:58 +0100

    liquibase was introduced to build child table and load test data into it

---
+++ child-service/database/reload-data.sh
@@ -0,0 +1,1 @@
+$LIQUIBASE_HOME/liquibase --classpath=$MYSQL_CONNECTOR_JAR --changeLogFile=child-db-testdata-changelog-master.xml update
--- child-service/src/main/resources/child.sql
+++
@@ -1,9 +0,0 @@
-create table child (
-    id int auto_increment primary key not null,
-    first_name varchar(50) not null,
-    surname varchar(50) not null,
-    date_of_birth date not null
-);
-
-insert into child (first_name, surname, date_of_birth) values ("Zsolt", "Balogh", str_to_date("07/07/1980", "%d/%m/%Y"));
-insert into child (first_name, surname, date_of_birth) values ("Dorina", "Dinnyesi", str_to_date("26/01/1981", "%d/%m/%Y"));
