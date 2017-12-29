6274844ddf63df0c5158b301ff0210b032745ae1
bzsolesz <zsolt.balogh99@gmail.com>
2017-12-29 00:32:36 +0000

    protractor-beautiful-reporter plugin was added to generate e2e test-report.html and capture screenshots for failed specs.

--- web/protractor.conf.js
+++ web/protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+var HtmlReporter = require('protractor-beautiful-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -24,5 +25,18 @@ exports.config = {
       project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+
+    jasmine.getEnv().addReporter(
+      new HtmlReporter({
+        baseDirectory: 'target/e2e',
+        screenshotsSubfolder: 'screenshots',
+        jsonsSubfolder: 'jsons',
+        takeScreenShotsOnlyForFailedSpecs: true,
+        docTitle: 'Capacity Planner Web e2e Protractor Test Execution Report',
+        docName: 'test-result.html',
+        preserveDirectory: true,
+        cssOverrideFile: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
+      }
+    ).getJasmine2Reporter());
   }
 };
