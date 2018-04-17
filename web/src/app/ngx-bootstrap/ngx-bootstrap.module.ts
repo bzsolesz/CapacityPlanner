63d0f9ef1df5b64e429312589e5c6ae76791e7cf
bzsolesz <zsolt.balogh99@gmail.com>
2018-04-17 23:05:33 +0100

    dark blue color theme for date picker to match bootstrap primary

--- web/src/app/ngx-bootstrap/ngx-bootstrap.module.ts
+++ web/src/app/ngx-bootstrap/ngx-bootstrap.module.ts
@@ -4,6 +4,7 @@ import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 import { BsDatepickerConfig } from "ngx-bootstrap/datepicker";
 
 export const defaultDatePickerConfig: Partial<BsDatepickerConfig> = {
+  containerClass: "theme-dark-blue",
   dateInputFormat: "DD/MM/YYYY",
   showWeekNumbers: false
 };
