ca39f799dc160cda8873140c3cbde02b3417865b
bzsolesz <zsolt.balogh99@gmail.com>
2018-10-07 20:22:22 +0100

    Updates to get build green

--- web/src/app/child/domain/weekly-attendance.stub.ts
+++ web/src/app/child/domain/weekly-attendance.stub.ts
@@ -1,15 +1,14 @@
 import { WeeklyAttendance } from ".";
 import { defaultDailyAttendance } from "./daily-attendance.stub";
-import { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY } from "../../shared/date";
 
 export function defaultWeeklyAttendance(overrides?: Partial<WeeklyAttendance>): WeeklyAttendance {
   const defaultObject: WeeklyAttendance = {
     id: 12345,
-    [MONDAY]: defaultDailyAttendance(),
-    [TUESDAY]: defaultDailyAttendance({from: "09:00"}),
-    [WEDNESDAY]: defaultDailyAttendance({from: "09:30"}),
-    [THURSDAY]: defaultDailyAttendance({to: "18:00"}),
-    [FRIDAY]: defaultDailyAttendance({to: "17:30"})
+    monday: defaultDailyAttendance(),
+    tuesday: defaultDailyAttendance({from: "09:00"}),
+    wednesday: defaultDailyAttendance({from: "09:30"}),
+    thursday: defaultDailyAttendance({to: "18:00"}),
+    friday: defaultDailyAttendance({to: "17:30"})
   };
   return { ...defaultObject, ...overrides };
 }
